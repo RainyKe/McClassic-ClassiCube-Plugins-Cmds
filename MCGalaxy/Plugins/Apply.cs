96f7c78b4e3d7a24027c5276d1f8f7d4971aed69
RainyKe <87867822+RainyKe@users.noreply.github.com>
2022-01-11 00:14:17 +0200

    Discord.cs

--- MCGalaxy/Plugins/Apply.cs
+++ MCGalaxy/Plugins/Apply.cs
@@ -1,6 +1,6 @@
 /*
-Apply - Provide the user with a link to apply for a new server with MCCH.
-@author Panda
+Discord - Gives you the discord server link
+@author OtiMalia12
 */
 using System;
 using System.IO;
@@ -10,36 +10,36 @@ using MCGalaxy.Commands;
 
 namespace MCGalaxy {
 
-    public class ApplyPlugin : Plugin {
-        public override string creator { get { return "Panda"; } }
+    public class DiscordPlugin : Plugin {
+        public override string creator { get { return "OtiMalia12"; } }
         public override string MCGalaxy_Version { get { return "1.9.1.2"; } }
-        public override string name { get { return "Apply"; } }
+        public override string name { get { return "Discord"; } }
 
         public override void Load(bool startup) {
             Command.Register(new CmdApply());
         }
 
         public override void Unload(bool shutdown) {
-            Command.Unregister(Command.Find("Apply"));
+            Command.Unregister(Command.Find("Discord"));
         }
     }
 
-    public sealed class CmdApply : Command {
-        public override string name { get { return "Apply"; } }
+    public sealed class CmdDiscord : Command {
+        public override string name { get { return "Discord"; } }
 		public override string shortcut { get { return "app"; } }
         public override string type { get { return "information"; } }
         public override bool museumUsable { get { return true; } }
         public override LevelPermission defaultRank { get { return LevelPermission.Guest; } }
 
         public override void Use(Player p, string message) {
-            p.Message("%SApply for a %anew%S server by clicking this link:");
-			p.Message("%bhttps://forms.gle/fcJFemh9fGQBbkSt5");
+            p.Message("%SJoin the %adiscord%S server by clicking this link:");
+			p.Message("%bhttps://discord.gg/gD7myXGXFw");
         }
 
         public override void Help(Player p)
 		{
-			p.Message("/Apply - Get the link to apply for a new server with MCCH.");
-			p.Message("Shortcut - /app");
+			p.Message("/Discord - Gives you the discord server link");
+			p.Message("Shortcut - /Ds");
 		}
     }
 }
